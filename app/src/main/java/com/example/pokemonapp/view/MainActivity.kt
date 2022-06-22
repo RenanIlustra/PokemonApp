13b32a06ca0a1d08259035e589c92133d7bf6ad4
RenanIlustra <renanrodrigues.ilustra@gmail.com>
2022-06-22 17:18:12 -0300

    RecyclerView config in MainActivity.kt

--- app/src/main/java/com/example/pokemonapp/view/MainActivity.kt
+++ app/src/main/java/com/example/pokemonapp/view/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.pokemonapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pokemonapp.R
+import com.example.pokemonapp.databinding.ActivityMainListBinding
+import com.example.pokemonapp.databinding.LayoutPokemonItem2Binding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_list)
+
+        val recyclerView = binding.rvPokemons
+
+        val linearLayoutManager = LinearLayoutManager(this)
     }
 }
