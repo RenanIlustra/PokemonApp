f3d75b2803dbe092169128d946b8872d94c3120b
RenanIlustra <renanrodrigues.ilustra@gmail.com>
2022-06-23 13:18:47 -0300

    Test RecyclerView with ViewBind

--- app/src/main/java/com/example/pokemonapp/domain/Pokemon.kt
+++ app/src/main/java/com/example/pokemonapp/domain/Pokemon.kt
@@ -1,6 +1,7 @@
 package com.example.pokemonapp.domain
 
 data class Pokemon(
+    val imgUrl :String,
     var name:String,
     var number:Int,
     val types: List<PokemonType>
--- app/src/main/java/com/example/pokemonapp/view/MainActivity.kt
+++ app/src/main/java/com/example/pokemonapp/view/MainActivity.kt
@@ -3,20 +3,37 @@ package com.example.pokemonapp.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pokemonapp.R
 import com.example.pokemonapp.databinding.ActivityMainListBinding
 import com.example.pokemonapp.databinding.LayoutPokemonItem2Binding
+import com.example.pokemonapp.domain.Pokemon
+import com.example.pokemonapp.domain.PokemonType
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainListBinding
+  //  lateinit var binding: ActivityMainListBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_list)
 
-        val recyclerView = binding.rvPokemons
+        val recyclerView = findViewById<RecyclerView>(R.id.rvPokemons)
+        //val recyclerView = binding.rvPokemons
+
+        val bulbasaur = Pokemon(
+            imgUrl = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+            name = "Bulbasaur",
+            number = 1,
+            types = listOf(PokemonType("Planta"))
+        )
+        val pokemons = listOf(bulbasaur,bulbasaur,bulbasaur,bulbasaur)
 
         val linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+        recyclerView.adapter = PokemonAdapter(pokemons)
     }
 }
