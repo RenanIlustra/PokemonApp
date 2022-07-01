541d90a690261274408b37e71ee5bf6742133181
RenanIlustra <renanrodrigues.ilustra@gmail.com>
2022-07-01 16:25:57 -0300

    mvvm

--- app/src/main/java/com/example/pokemonapp/model/SinglePokemon.kt
+++ app/src/main/java/com/example/pokemonapp/model/SinglePokemon.kt
@@ -1,9 +1,7 @@
 package com.example.pokemonapp.model
 
-import com.example.pokemonapp.X.apiX.models.PokemonTypeSlot
-
 data class SinglePokemon (
         val id: Int,
         val name: String,
-        val types: PokemonTypeSlot
+        val types: TypeSlot
         )
